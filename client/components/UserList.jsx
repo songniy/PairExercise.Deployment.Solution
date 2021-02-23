8427beb5bfdacdce390a389bfd8b331d3a8dce9c
Yin Song <songniy@gmail.com>
2021-02-23 15:52:02 -0500

    testing travis integration. my repo, not fullstack repo

--- client/components/UserList.jsx
+++ client/components/UserList.jsx
@@ -5,7 +5,8 @@ export default ({ users }) => (
   <div className='user-list'>
     <p className='lead'>
       Here's a list of {users.length} completely fictitious people: some text to
-      test travis CI More stuff added to test again
+      test travis CI More stuff added to test again. remove bash test and going
+      back to windows test
     </p>
 
     <table className='table'>
